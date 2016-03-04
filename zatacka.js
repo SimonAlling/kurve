1a3be49135fcaee9c67e9dcb3b8d0418347b7970
Simon Alling <alling.simon@gmail.com>
2016-03-04 17:27:30 +0100

    Make spawnArea a function of spawnMargin

--- zatacka.js
+++ zatacka.js
@@ -10,14 +10,13 @@ var pixels = new Array(canvasWidth*canvasHeight).fill(0);
 
 var KEY = Object.freeze({ BACKSPACE: 8, TAB: 9, ENTER: 13, SHIFT: 16, CTRL: 17, ALT: 18, PAUSE: 19, CAPS_LOCK: 20, ESCAPE: 27, SPACE: 32, PAGE_UP: 33, PAGE_DOWN: 34, END: 35, HOME: 36, LEFT_ARROW: 37, UP_ARROW: 38, RIGHT_ARROW: 39, DOWN_ARROW: 40, INSERT: 45, DELETE: 46, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, A: 65, B: 66, C: 67, D: 68, E: 69, F: 70, G: 71, H: 72, I: 73, J: 74, K: 75, L: 76, M: 77, N: 78, O: 79, P: 80, Q: 81, R: 82, S: 83, T: 84, U: 85, V: 86, W: 87, X: 88, Y: 89, Z: 90, LEFT_META: 91, RIGHT_META: 92, SELECT: 93, NUMPAD_0: 96, NUMPAD_1: 97, NUMPAD_2: 98, NUMPAD_3: 99, NUMPAD_4: 100, NUMPAD_5: 101, NUMPAD_6: 102, NUMPAD_7: 103, NUMPAD_8: 104, NUMPAD_9: 105, MULTIPLY: 106, ADD: 107, SUBTRACT: 109, DECIMAL: 110, DIVIDE: 111, F1: 112, F2: 113, F3: 114, F4: 115, F5: 116, F6: 117, F7: 118, F8: 119, F9: 120, F10: 121, F11: 122, F12: 123, NUM_LOCK: 144, SCROLL_LOCK: 145, SEMICOLON: 186, EQUALS: 187, COMMA: 188, DASH: 189, PERIOD: 190, FORWARD_SLASH: 191, GRAVE_ACCENT: 192, OPEN_BRACKET: 219, BACK_SLASH: 220, CLOSE_BRACKET: 221, SINGLE_QUOTE: 222 });
 
-var config = {
+const config = Object.freeze({
     tickrate: 600, // Hz
     drawrate: 60, // Hz
     kurveThickness: 3,
     minSpawnAngle: -Math.PI/2,
     maxSpawnAngle:  Math.PI/2,
     spawnMargin: 100,
-    spawnArea: null,
     maxPlayers: 6,
     speed: 64, // Kuxels per second
     turningRadius: 27, // Kuxels (NB: _radius_)
@@ -32,9 +31,8 @@ var config = {
         { name: "Pink",   color: "#DF51B6", keyL: KEY.DIVIDE, keyR: KEY.MULTIPLY },
         { name: "Blue",   color: "#00A2CB", keyL: null, keyR: null }
     ]
-};
+});
 
-config.spawnArea = computeSpawnArea(config.spawnMargin);
 var ticksSinceDraw = 0;
 var maxTicksBeforeDraw = config.tickrate/config.drawrate;
 
@@ -170,9 +168,10 @@ function getPixels(left, top) {
 }
 
 function generateSpawnPosition() {
+    var spawnArea = computeSpawnArea(config.spawnMargin);
     return {
-        x: randomFloat(config.spawnArea.x_min, config.spawnArea.x_max),
-        y: randomFloat(config.spawnArea.y_min, config.spawnArea.y_max)
+        x: randomFloat(spawnArea.x_min, spawnArea.x_max),
+        y: randomFloat(spawnArea.y_min, spawnArea.y_max)
     };
 }
 
