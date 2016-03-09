dbbe4f3c031b7276f8a951e465f50edbaad578f9
Simon Alling <alling.simon@gmail.com>
2016-03-09 21:21:39 +0100

    Fix major tickrate/draw issues
    It would sometimes happen that a player would die just before actually
    bumping into anything. The reason for this was that maxTicksBeforeDraw was
    incorrectly calculated. I suspect that the reason why this would occur
    pretty seldom (I'm taking a blind guess at a 4/64 change), and why the
    error would never be more than 1 Kuxel, is that the max allowed framerate
    (60 fps) was very close to the Kurve speed (64 Kx/s).
    
    This commit rethinks the update/draw loop such that the max number of
    ticks between draws is calculated as the tickrate over the speed, e.g.
    600 Hz / 64 Kx/s = 9.375, rounded down to the closest integer, or 1 if <1.

--- zatacka.js
+++ zatacka.js
@@ -13,8 +13,7 @@ var pixels = new Array(canvasWidth*canvasHeight).fill(0);
 var KEY = Object.freeze({ BACKSPACE: 8, TAB: 9, ENTER: 13, SHIFT: 16, CTRL: 17, ALT: 18, PAUSE: 19, CAPS_LOCK: 20, ESCAPE: 27, SPACE: 32, PAGE_UP: 33, PAGE_DOWN: 34, END: 35, HOME: 36, LEFT_ARROW: 37, UP_ARROW: 38, RIGHT_ARROW: 39, DOWN_ARROW: 40, INSERT: 45, DELETE: 46, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, A: 65, B: 66, C: 67, D: 68, E: 69, F: 70, G: 71, H: 72, I: 73, J: 74, K: 75, L: 76, M: 77, N: 78, O: 79, P: 80, Q: 81, R: 82, S: 83, T: 84, U: 85, V: 86, W: 87, X: 88, Y: 89, Z: 90, LEFT_META: 91, RIGHT_META: 92, SELECT: 93, NUMPAD_0: 96, NUMPAD_1: 97, NUMPAD_2: 98, NUMPAD_3: 99, NUMPAD_4: 100, NUMPAD_5: 101, NUMPAD_6: 102, NUMPAD_7: 103, NUMPAD_8: 104, NUMPAD_9: 105, MULTIPLY: 106, ADD: 107, SUBTRACT: 109, DECIMAL: 110, DIVIDE: 111, F1: 112, F2: 113, F3: 114, F4: 115, F5: 116, F6: 117, F7: 118, F8: 119, F9: 120, F10: 121, F11: 122, F12: 123, NUM_LOCK: 144, SCROLL_LOCK: 145, SEMICOLON: 186, EQUALS: 187, COMMA: 188, DASH: 189, PERIOD: 190, FORWARD_SLASH: 191, GRAVE_ACCENT: 192, OPEN_BRACKET: 219, BACK_SLASH: 220, CLOSE_BRACKET: 221, SINGLE_QUOTE: 222 });
 
 const config = Object.freeze({
-    tickrate: 600, // Hz
-    drawrate: 60, // Hz
+    tickrate: 600, // Hz (absolute minimum: 30)
     kurveThickness: 3,
     minSpawnAngle: -Math.PI/2,
     maxSpawnAngle:  Math.PI/2,
@@ -40,8 +39,22 @@ var defaultPlayers = [
     { name: "Blue"  , color: "#00A2CB", keyL: null          , keyR: null           }
 ];
 
-var ticksSinceDraw = 0;
-var maxTicksBeforeDraw = config.tickrate/config.drawrate;
+var totalNumberOfTicks = 0;
+var maxTicksBeforeDraw = Math.max(Math.floor(config.tickrate/config.speed), 1);
+
+// maxTicksBeforeDraw exists because we do not want to queue a large number of unnecessary draws.
+// It is calculated like so:
+// We cannot allow a Kurve to travel more than 1 Kuxel without a draw, since that gives us the bug
+// where it looks like you die before actually hitting an obstacle.
+// s = vt (where s is 1 Kuxel and v is known)
+// t = s / v = 1 / v
+// t is the number of seconds we can wait before we have to draw.
+// t equals some number of ticks, n, times the time of one tick, 1 / f:
+// t = n / f (where f is the tickrate and n is the sought maxTicksBeforeDraw)
+// 1 / v = n / f
+// n = f / v
+// We then Math.floor() it so we can do modulo calculations with it and because integers are nice.
+
 
 function init() {
 
@@ -238,7 +251,7 @@ function update(delta) {
     for (var i = 0, len = game.livePlayers.length; i < len; i++) {
         game.livePlayers[i].update(delta);
     }
-    ticksSinceDraw++;
+    totalNumberOfTicks++;
 }
 
 /**
@@ -575,7 +588,7 @@ Player.prototype.update = function(delta) {
     var theta = this.velocity * delta / 1000;
     this.x = this.x + theta * Math.cos(this.direction);
     this.y = this.y - theta * Math.sin(this.direction);
-    if (this.isAlive() && ticksSinceDraw % maxTicksBeforeDraw === 0) {
+    if (this.isAlive() && totalNumberOfTicks % maxTicksBeforeDraw === 0) {
         this.queuedDraws.enqueue({"x": this.x, "y": this.y });
     }
 };
