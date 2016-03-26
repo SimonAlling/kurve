4be3a204da92bc610eb72f7cd7ef5055c55e7a9d
Simon Alling <alling.simon@gmail.com>
2016-03-26 06:39:25 +0100

    Fix top-level indentation

--- GUIController.js
+++ GUIController.js
@@ -2,113 +2,113 @@
 
 function GUIController(cfg) {
 
-const CLASS_ACTIVE = "active";
-const CLASS_HIDDEN = "hidden";
+    const CLASS_ACTIVE = "active";
+    const CLASS_HIDDEN = "hidden";
 
-const config = cfg;
-const lobby = byID("lobby");
-const controls = byID("controls");
-const scoreboard = byID("scoreboard");
-const results = byID("results");
-const konecHry = byID("KONEC_HRY");
+    const config = cfg;
+    const lobby = byID("lobby");
+    const controls = byID("controls");
+    const scoreboard = byID("scoreboard");
+    const results = byID("results");
+    const konecHry = byID("KONEC_HRY");
 
 
 
-// PRIVATE FUNCTIONS
+    // PRIVATE FUNCTIONS
 
-function hideLobby() {
-    log("Hiding lobby.");
-    lobby.classList.add(CLASS_HIDDEN);
-}
+    function hideLobby() {
+        log("Hiding lobby.");
+        lobby.classList.add(CLASS_HIDDEN);
+    }
 
-function showScoreOfPlayer(id) {
-    var index = id - 1;
-    if (isHTMLElement(scoreboard)) {
-        var scoreboardEntry = scoreboard.children[index];
-        if (isHTMLElement(scoreboardEntry)) {
-            scoreboardEntry.classList.add("active");
+    function showScoreOfPlayer(id) {
+        var index = id - 1;
+        if (isHTMLElement(scoreboard)) {
+            var scoreboardEntry = scoreboard.children[index];
+            if (isHTMLElement(scoreboardEntry)) {
+                scoreboardEntry.classList.add("active");
+            }
+        }
+        if (isHTMLElement(results)) {
+            var resultsEntry = results.children[index];
+            if (isHTMLElement(resultsEntry)) {
+                resultsEntry.classList.add("active");
+            }
         }
     }
-    if (isHTMLElement(results)) {
-        var resultsEntry = results.children[index];
-        if (isHTMLElement(resultsEntry)) {
-            resultsEntry.classList.add("active");
-        }
+
+
+    // PUBLIC API
+
+    function playerReady(id) {
+    	var index = id - 1;
+    	try {
+    		controls.children[index].children[1].classList.add(CLASS_ACTIVE);
+    	} catch (error) {
+    		console.error(error);
+    	}
     }
-}
 
+    function playerUnready(id) {
+    	var index = id - 1;
+    	try {
+    		controls.children[index].children[1].classList.remove(CLASS_ACTIVE);
+    	} catch (error) {
+    		console.error(error);
+    	}
+    }
 
-// PUBLIC API
+    function gameStarted() {
+    	hideLobby();
+    }
 
-function playerReady(id) {
-	var index = id - 1;
-	try {
-		controls.children[index].children[1].classList.add(CLASS_ACTIVE);
-	} catch (error) {
-		console.error(error);
-	}
-}
+    function initScoreOfPlayer(id) {
+    	updateScoreOfPlayer(id, 0);
+    	showScoreOfPlayer(id);
+    }
 
-function playerUnready(id) {
-	var index = id - 1;
-	try {
-		controls.children[index].children[1].classList.remove(CLASS_ACTIVE);
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-function gameStarted() {
-	hideLobby();
-}
-
-function initScoreOfPlayer(id) {
-	updateScoreOfPlayer(id, 0);
-	showScoreOfPlayer(id);
-}
-
-function updateScoreOfPlayer(id, newScore) {
-    if (!isHTMLElement(scoreboard)) {
-        logError("Scoreboard HTML element could not be found.");
-    } else if (!isHTMLElement(results)) {
-        logError("Results HTML element could not be found.");
-    } else {
-        let scoreboardItem = scoreboard.children[id-1];      // minus 1 necessary since player IDs are 1-indexed
-        let resultsItem = results.children[id-1];            // minus 1 necessary since player IDs are 1-indexed
-        let onesDigit = newScore % 10;                       // digit at the ones position (4 in 14)
-        let tensDigit = (newScore - (newScore % 10)) / 10;   // digit at the tens position (1 in 14)
-
-        // Scoreboard:
-        if (isHTMLElement(scoreboardItem) && isHTMLElement(scoreboardItem.children[0]) && isHTMLElement(scoreboardItem.children[1])) {
-            // The digit elements are ordered such that children[0] is ones, children[1] is tens, and so on.
-            // First, we have to remove all digit classes:
-            scoreboardItem.children[0].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
-            scoreboardItem.children[1].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
-            // Add appropriate classes to tens and ones position, respectively:
-            scoreboardItem.children[0].classList.add("d"+tensDigit);
-            scoreboardItem.children[1].classList.add("d"+onesDigit);
+    function updateScoreOfPlayer(id, newScore) {
+        if (!isHTMLElement(scoreboard)) {
+            logError("Scoreboard HTML element could not be found.");
+        } else if (!isHTMLElement(results)) {
+            logError("Results HTML element could not be found.");
         } else {
-            logError("Could not find HTML scoreboard entry for player "+id+".");
-        }
+            let scoreboardItem = scoreboard.children[id-1];      // minus 1 necessary since player IDs are 1-indexed
+            let resultsItem = results.children[id-1];            // minus 1 necessary since player IDs are 1-indexed
+            let onesDigit = newScore % 10;                       // digit at the ones position (4 in 14)
+            let tensDigit = (newScore - (newScore % 10)) / 10;   // digit at the tens position (1 in 14)
 
-        // Results:
-        if (isHTMLElement(resultsItem) && isHTMLElement(resultsItem.children[0]) && isHTMLElement(resultsItem.children[1])) {
-            resultsItem.children[0].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
-            resultsItem.children[1].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
-            resultsItem.children[0].classList.add("d"+tensDigit);
-            resultsItem.children[1].classList.add("d"+onesDigit);
-        } else {
-            logError("Could not find HTML results entry for player "+id+".");
+            // Scoreboard:
+            if (isHTMLElement(scoreboardItem) && isHTMLElement(scoreboardItem.children[0]) && isHTMLElement(scoreboardItem.children[1])) {
+                // The digit elements are ordered such that children[0] is ones, children[1] is tens, and so on.
+                // First, we have to remove all digit classes:
+                scoreboardItem.children[0].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
+                scoreboardItem.children[1].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
+                // Add appropriate classes to tens and ones position, respectively:
+                scoreboardItem.children[0].classList.add("d"+tensDigit);
+                scoreboardItem.children[1].classList.add("d"+onesDigit);
+            } else {
+                logError("Could not find HTML scoreboard entry for player "+id+".");
+            }
+
+            // Results:
+            if (isHTMLElement(resultsItem) && isHTMLElement(resultsItem.children[0]) && isHTMLElement(resultsItem.children[1])) {
+                resultsItem.children[0].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
+                resultsItem.children[1].classList.remove("d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9");
+                resultsItem.children[0].classList.add("d"+tensDigit);
+                resultsItem.children[1].classList.add("d"+onesDigit);
+            } else {
+                logError("Could not find HTML results entry for player "+id+".");
+            }
         }
     }
-}
 
-return {
-	playerReady: playerReady,
-	playerUnready: playerUnready,
-	gameStarted: gameStarted,
-	initScoreOfPlayer: initScoreOfPlayer,
-	updateScoreOfPlayer: updateScoreOfPlayer
-};
+    return {
+    	playerReady: playerReady,
+    	playerUnready: playerUnready,
+    	gameStarted: gameStarted,
+    	initScoreOfPlayer: initScoreOfPlayer,
+    	updateScoreOfPlayer: updateScoreOfPlayer
+    };
 
 }
--- zatacka.js
+++ zatacka.js
@@ -2,135 +2,135 @@
 
 const Zatacka = (function(window, document) {
 
-const canvas = byID("canvas");
+    const canvas = byID("canvas");
 
-const config = Object.freeze({
-    tickrate: 600, // Hz
-    maxFramerate: 60, // Hz
-    width: 559, // Kuxels
-    height: 480, // Kuxels
-    thickness: 3, // Kuxels
-    speed: 60, // Kuxels per second
-    turningRadius: 27, // Kuxels (NB: _radius_)
-    minSpawnAngle: -Math.PI/2, // radians
-    maxSpawnAngle:  Math.PI/2, // radians
-    spawnMargin: 100, // Kuxels
-    flickerFrequency: 20, // Hz, when spawning
-    flickerDuration: 830, // ms, when spawning
-    minHoleDistance: 90, // Kuxels
-    maxHoleDistance: 300, // Kuxels
-    minHoleLength: 8, // Kuxels
-    maxHoleLength: 12, // Kuxels
-    keys: {
-        "proceed": [KEY.SPACE, KEY.ENTER],
-        "quit":    [KEY.ESCAPE]
-    },
-    defaultPlayers: Object.freeze([
-	    { id: 1, name: "Red"   , color: "#FF2800", keyL: KEY["1"]      , keyR: KEY.Q          },
-	    { id: 2, name: "Yellow", color: "#C3C300", keyL: KEY.CTRL      , keyR: KEY.ALT        },
-	    { id: 3, name: "Orange", color: "#FF7900", keyL: KEY.M         , keyR: KEY.COMMA      },
-	    { id: 4, name: "Green" , color: "#00CB00", keyL: KEY.LEFT_ARROW, keyR: KEY.DOWN_ARROW },
-	    { id: 5, name: "Pink"  , color: "#DF51B6", keyL: KEY.DIVIDE    , keyR: KEY.MULTIPLY   },
-	    { id: 6, name: "Blue"  , color: "#00A2CB", keyL: KEY.C         , keyR: KEY.V          }
-	])
-});
+    const config = Object.freeze({
+        tickrate: 600, // Hz
+        maxFramerate: 60, // Hz
+        width: 559, // Kuxels
+        height: 480, // Kuxels
+        thickness: 3, // Kuxels
+        speed: 60, // Kuxels per second
+        turningRadius: 27, // Kuxels (NB: _radius_)
+        minSpawnAngle: -Math.PI/2, // radians
+        maxSpawnAngle:  Math.PI/2, // radians
+        spawnMargin: 100, // Kuxels
+        flickerFrequency: 20, // Hz, when spawning
+        flickerDuration: 830, // ms, when spawning
+        minHoleDistance: 90, // Kuxels
+        maxHoleDistance: 300, // Kuxels
+        minHoleLength: 8, // Kuxels
+        maxHoleLength: 12, // Kuxels
+        keys: {
+            "proceed": [KEY.SPACE, KEY.ENTER],
+            "quit":    [KEY.ESCAPE]
+        },
+        defaultPlayers: Object.freeze([
+    	    { id: 1, name: "Red"   , color: "#FF2800", keyL: KEY["1"]      , keyR: KEY.Q          },
+    	    { id: 2, name: "Yellow", color: "#C3C300", keyL: KEY.CTRL      , keyR: KEY.ALT        },
+    	    { id: 3, name: "Orange", color: "#FF7900", keyL: KEY.M         , keyR: KEY.COMMA      },
+    	    { id: 4, name: "Green" , color: "#00CB00", keyL: KEY.LEFT_ARROW, keyR: KEY.DOWN_ARROW },
+    	    { id: 5, name: "Pink"  , color: "#DF51B6", keyL: KEY.DIVIDE    , keyR: KEY.MULTIPLY   },
+    	    { id: 6, name: "Blue"  , color: "#00A2CB", keyL: KEY.C         , keyR: KEY.V          }
+    	])
+    });
 
-function isProceedKey(key) {
-    return config.keys.proceed.indexOf(key) !== -1;
-}
+    function isProceedKey(key) {
+        return config.keys.proceed.indexOf(key) !== -1;
+    }
 
-function isQuitKey(key) {
-    return config.keys.quit.indexOf(key) !== -1;
-}
+    function isQuitKey(key) {
+        return config.keys.quit.indexOf(key) !== -1;
+    }
 
-function defaultPlayer(id) {
-    var dp;
-    for (var i = 0; i < config.defaultPlayers.length; i++) {
-        dp = config.defaultPlayers[i];
-        if (dp.id === id) {
-            return new Player(dp.id, dp.name, dp.color, dp.keyL, dp.keyR);
+    function defaultPlayer(id) {
+        var dp;
+        for (var i = 0; i < config.defaultPlayers.length; i++) {
+            dp = config.defaultPlayers[i];
+            if (dp.id === id) {
+                return new Player(dp.id, dp.name, dp.color, dp.keyL, dp.keyR);
+            }
         }
     }
-}
 
-function proceedKeyPressedInLobby() {
-    var numberOfReadyPlayers = game.getNumberOfActivePlayers();
-    if (numberOfReadyPlayers > 0) {
-        removeLobbyEventListeners();
-        addGameEventListeners();
-        game.setMode(numberOfReadyPlayers === 1 ? Game.PRACTICE : Game.COMPETITIVE);
-        game.start();
-    }
-}
-
-function keyPressedInLobby(pressedKey) {
-    for (var i = 0; i < config.defaultPlayers.length; i++) {
-        let player = config.defaultPlayers[i];
-        if (pressedKey === player.keyL) {
-            game.addPlayer(defaultPlayer(player.id));
-        } else if (pressedKey === player.keyR) {
-            game.removePlayer(player.id);
+    function proceedKeyPressedInLobby() {
+        var numberOfReadyPlayers = game.getNumberOfActivePlayers();
+        if (numberOfReadyPlayers > 0) {
+            removeLobbyEventListeners();
+            addGameEventListeners();
+            game.setMode(numberOfReadyPlayers === 1 ? Game.PRACTICE : Game.COMPETITIVE);
+            game.start();
         }
     }
-}
 
-function lobbyKeyHandler() {
-    let pressedKey = window.event.keyCode;
-    if (isProceedKey(pressedKey)) {
-        proceedKeyPressedInLobby();
-    } else {
-        keyPressedInLobby(pressedKey);
+    function keyPressedInLobby(pressedKey) {
+        for (var i = 0; i < config.defaultPlayers.length; i++) {
+            let player = config.defaultPlayers[i];
+            if (pressedKey === player.keyL) {
+                game.addPlayer(defaultPlayer(player.id));
+            } else if (pressedKey === player.keyR) {
+                game.removePlayer(player.id);
+            }
+        }
     }
-}
 
-function lobbyMouseHandler() {
+    function lobbyKeyHandler() {
+        let pressedKey = window.event.keyCode;
+        if (isProceedKey(pressedKey)) {
+            proceedKeyPressedInLobby();
+        } else {
+            keyPressedInLobby(pressedKey);
+        }
+    }
 
-}
+    function lobbyMouseHandler() {
 
-function gameKeyHandler() {
-    game.keyHandler(window.event.keyCode);
-}
+    }
 
-function gameMouseHandler() {
+    function gameKeyHandler() {
+        game.keyHandler(window.event.keyCode);
+    }
 
-}
+    function gameMouseHandler() {
 
-function addLobbyEventListeners() {
-    log("Adding lobby event listeners ...");
-    document.addEventListener("keydown", lobbyKeyHandler);
-    document.addEventListener("mousedown", lobbyMouseHandler);
-    log("Done.");
-}
+    }
 
-function removeLobbyEventListeners() {
-    log("Removing lobby event listeners ...");
-    document.removeEventListener("keydown", lobbyKeyHandler);
-    document.removeEventListener("mousedown", lobbyMouseHandler);
-    log("Done.");
-}
+    function addLobbyEventListeners() {
+        log("Adding lobby event listeners ...");
+        document.addEventListener("keydown", lobbyKeyHandler);
+        document.addEventListener("mousedown", lobbyMouseHandler);
+        log("Done.");
+    }
 
-function addGameEventListeners() {
-    log("Adding game event listeners ...");
-    document.addEventListener("keydown", Keyboard.onKeydown.bind(Keyboard));
-    document.addEventListener("keyup", Keyboard.onKeyup.bind(Keyboard));
-    document.addEventListener("keydown", gameKeyHandler);
-    document.addEventListener("mousedown", gameMouseHandler);
-    log("Done.");
-}
+    function removeLobbyEventListeners() {
+        log("Removing lobby event listeners ...");
+        document.removeEventListener("keydown", lobbyKeyHandler);
+        document.removeEventListener("mousedown", lobbyMouseHandler);
+        log("Done.");
+    }
 
-function removeGameEventListeners() {
-    log("Removing game event listeners ...");
-    document.removeEventListener("keydown", gameKeyHandler);
-    document.removeEventListener("mousedown", gameMouseHandler);
-    log("Done.");
-}
+    function addGameEventListeners() {
+        log("Adding game event listeners ...");
+        document.addEventListener("keydown", Keyboard.onKeydown.bind(Keyboard));
+        document.addEventListener("keyup", Keyboard.onKeyup.bind(Keyboard));
+        document.addEventListener("keydown", gameKeyHandler);
+        document.addEventListener("mousedown", gameMouseHandler);
+        log("Done.");
+    }
 
-addLobbyEventListeners();
+    function removeGameEventListeners() {
+        log("Removing game event listeners ...");
+        document.removeEventListener("keydown", gameKeyHandler);
+        document.removeEventListener("mousedown", gameMouseHandler);
+        log("Done.");
+    }
 
-const game = new Game(config, Renderer(config, canvas), GUIController(config));
+    addLobbyEventListeners();
 
-return {
-	getConfig: function() { return config; }
-};
+    const game = new Game(config, Renderer(config, canvas), GUIController(config));
+
+    return {
+    	getConfig: function() { return config; }
+    };
 
 })(window, document);
