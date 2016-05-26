061f55256a5e508679eb172a7fb7951a4f385fee
Simon Alling <alling.simon@gmail.com>
2016-05-26 22:55:55 +0200

    Improve CSS generator

--- dev/css-generator.js
+++ dev/css-generator.js
@@ -1,29 +1,31 @@
-function generateCSS() {
+function generateCSS(maxScaling) {
 	const defaultWidth = 640;
 	const defaultHeight = 480;
 	const leftWidth = 80;
-	const maxScaling = 5;
+	const defaultMaxScaling = 8;
+	const safeMaxScaling = maxScaling || defaultMaxScaling;
 
 	let css = "";
-	for (let i = 1; i < maxScaling; i++) {
+	for (let i = 1; i <= safeMaxScaling; i++) {
 		css += `/* ======== ${i}x ======== */
-		@media screen and (min-width: ${i*defaultWidth}px) and (min-height: ${i*defaultHeight}px) {
-		    body { justify-content: flex-end; }
-		    #wrapper {
-		        transform: scale(${i});
-		        transform-origin: right;
-		    }
-		}
+@media screen and (min-width: ${i*defaultWidth}px) and (min-height: ${i*defaultHeight}px) {
+    body { justify-content: flex-end; }
+    #wrapper {
+        zoom: ${i};
+        -moz-transform: scale(${i});
+        transform-origin: right;
+    }
+}
 
-		@media screen and (min-width: ${i*(defaultWidth+leftWidth)}px) and (min-height: ${i*defaultHeight}px) {
-		    body { justify-content: center; }
-		    #wrapper {
-		        transform-origin: center;
-		    }
-		}
+@media screen and (min-width: ${i*(defaultWidth+leftWidth)}px) and (min-height: ${i*defaultHeight}px) {
+    body { justify-content: center; }
+    #wrapper {
+        transform-origin: center;
+    }
+}
 
 
-		`;
+`;
 	}
 
 	return css;
