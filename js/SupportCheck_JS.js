dad3b044e064d926ae475c911ab9cf29847a02ba
Simon Alling <alling.simon@gmail.com>
2016-06-04 22:21:37 +0200

    Add JS and ES6 compatibility checks
    They need to be in separate files; otherwise, browsers without ES6 support
    may fail to hide the JS enabled error message.

---
+++ js/SupportCheck_JS.js
@@ -0,0 +1,17 @@
+// Hide the JavaScript error message:
+(function() {
+	var css = "#fatalError_JS { display: none; }";
+	var styleElement = document.createElement("style");
+	styleElement.innerHTML = css;
+	document.head.appendChild(styleElement);
+})();
+
+
+
+// Show the ES6 error message:
+(function() {
+	var css = "#fatalError_ES6 { display: table; }";
+	var styleElement = document.createElement("style");
+	styleElement.innerHTML = css;
+	document.head.appendChild(styleElement);
+})();
