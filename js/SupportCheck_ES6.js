e49cc19375ed2d206f59d631fe92b7c3099f2220
Simon Alling <alling.simon@gmail.com>
2016-06-05 14:19:56 +0200

    Convert indentation from tabs to spaces

--- js/SupportCheck_ES6.js
+++ js/SupportCheck_ES6.js
@@ -3,19 +3,19 @@
 
 // Hide ES6 error message if browser passes ES6 support check:
 ((x = `x`, y = `${x}`) => {
-	let a = "a";
-	const b = "b";
-	if (
-			y === x &&
-			Object.freeze &&
-			Array.prototype.map &&
-			Array.prototype.some &&
-			Array.prototype.find &&
-			Array.prototype.forEach
-		) {
-		const css = "#fatalError_ES6 { display: none; }";
-		const styleElement = document.createElement("style");
-		styleElement.textContent = css;
-		document.head.appendChild(styleElement);
-	}
+    let a = "a";
+    const b = "b";
+    if (
+            y === x &&
+            Object.freeze &&
+            Array.prototype.map &&
+            Array.prototype.some &&
+            Array.prototype.find &&
+            Array.prototype.forEach
+        ) {
+        const css = "#fatalError_ES6 { display: none; }";
+        const styleElement = document.createElement("style");
+        styleElement.textContent = css;
+        document.head.appendChild(styleElement);
+    }
 })();
