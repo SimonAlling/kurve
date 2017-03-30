19057a5f28e6fe8dca6add0289a436934ccd9ea7
Simon Alling <alling.simon@gmail.com>
2017-03-30 19:34:03 +0200

    Improve formatting in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,30 @@ const path = require("path");
 const DIR_SOURCE = "js";
 
 module.exports = {
-  devtool: "source-map",
-  entry: {
-    zatacka: "./" + DIR_SOURCE + "/Main.js",
-    splashscreen: "./" + DIR_SOURCE + "/SplashScreen.js",
-},
-  output: {
-    path: path.resolve(__dirname, "."),
-    filename: "[name].min.js"
-  },
-  module: {
-    loaders: [
-      {
-        loader: "babel-loader",
-
-        // Skip any files outside of your project"s source directory
-        include: [
-          path.resolve(__dirname, DIR_SOURCE),
-        ],
-
-        // Only run `.js` files through Babel
-        test: /\.js$/,
-
-        // Options to configure babel with
-        query: {
-          presets: ["es2015"],
-        }
-      },
-    ]
-  }
+    devtool: "source-map",
+    entry: {
+        zatacka: "./" + DIR_SOURCE + "/Main.js",
+        splashscreen: "./" + DIR_SOURCE + "/SplashScreen.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "."),
+        filename: "[name].min.js"
+    },
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+                // Skip any files outside of your project"s source directory:
+                include: [
+                    path.resolve(__dirname, DIR_SOURCE),
+                ],
+                // Only run `.js` files through Babel:
+                test: /\.js$/,
+                // Options to configure babel with:
+                query: {
+                    presets: ["es2015"],
+                }
+            },
+        ]
+    }
 };
