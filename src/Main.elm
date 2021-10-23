2ebea86f1a939ef816426ae8620617d42986beaa
Simon Alling <alling.simon@gmail.com>
2021-10-23 12:33:08 +0200

    Define speed, turning radius etc

--- src/Main.elm
+++ src/Main.elm
@@ -14,9 +14,48 @@ port onKeydown : (String -> msg) -> Sub msg
 port onKeyup : (String -> msg) -> Sub msg
 
 
-tickrate : Float
-tickrate =
-    60
+{-| A tickrate in ticks per second (Hz).
+-}
+type Tickrate
+    = Tickrate Float
+
+
+tickrateToFloat : Tickrate -> Float
+tickrateToFloat (Tickrate r) =
+    r
+
+
+{-| A speed in kuxels per second.
+-}
+type Speed
+    = Speed Float
+
+
+speedToFloat : Speed -> Float
+speedToFloat (Speed s) =
+    s
+
+
+{-| An angle in radians.
+-}
+type Angle
+    = Angle Float
+
+
+angleToFloat : Angle -> Float
+angleToFloat (Angle a) =
+    a
+
+
+{-| A radius in kuxels.
+-}
+type Radius
+    = Radius Float
+
+
+radiusToFloat : Radius -> Float
+radiusToFloat (Radius r) =
+    r
 
 
 type alias Model =
@@ -44,31 +83,49 @@ type Msg
     | KeyWasReleased String
 
 
+theTickrate : Tickrate
+theTickrate =
+    Tickrate 60
+
+
+theTurningRadius : Radius
+theTurningRadius =
+    -- Kuxels (NB: radius, not diameter)
+    Radius 28.5
+
+
+theSpeed : Speed
+theSpeed =
+    Speed 60
+
+
+theAngleChange : Angle
+theAngleChange =
+    Angle (speedToFloat theSpeed / (tickrateToFloat theTickrate * radiusToFloat theTurningRadius))
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick _ ->
             let
-                theta =
-                    1
-
-                angleChange =
-                    0.1
+                distanceTraveledSinceLastTick =
+                    speedToFloat theSpeed / tickrateToFloat theTickrate
 
                 newDirection =
                     if Set.member "m" model.pressedKeys then
-                        model.direction + angleChange
+                        model.direction + angleToFloat theAngleChange
 
                     else if Set.member "," model.pressedKeys then
-                        model.direction - angleChange
+                        model.direction - angleToFloat theAngleChange
 
                     else
                         model.direction
 
                 newModel =
                     { model
-                        | x = model.x + theta * cos newDirection
-                        , y = model.y - theta * sin newDirection
+                        | x = model.x + distanceTraveledSinceLastTick * cos newDirection
+                        , y = model.y - distanceTraveledSinceLastTick * sin newDirection
                         , direction = newDirection
                     }
             in
@@ -97,7 +154,7 @@ integerCoordinates coords =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
-        [ Time.every (1000 / tickrate) Tick
+        [ Time.every (1000 / tickrateToFloat theTickrate) Tick
         , onKeydown KeyWasPressed
         , onKeyup KeyWasReleased
         ]
