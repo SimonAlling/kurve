804eff33b3a83aecf6ad7d93cd4e97fb2d563a08
Simon Alling <SimonAlling@users.noreply.github.com>
2023-01-03 18:03:25 +0100

    Add real end screen (#60)
    This PR replaces the placeholder end screen added in #59 with an actual
    end screen showing the final results, along with the traditional
    pixelated "KONEC HRY" text.
    
    The elements of the end screen are placed so as to pixel-perfectly match
    the legacy JavaScript version; hence the seemingly arbitrary `px`
    dimensions in the stylesheet.
    
    💡 `git show --color-words='Html\.text.+|.'`
    
    Co-authored-by: Simon Lydell <simon.lydell@gmail.com>

--- src/GUI/EndScreen.elm
+++ src/GUI/EndScreen.elm
@@ -1,13 +1,42 @@
 module GUI.EndScreen exposing (endScreen)
 
+import Dict
+import GUI.Digits
 import Html exposing (Html, div)
 import Html.Attributes as Attr
+import Players exposing (AllPlayers)
+import Types.Player exposing (Player)
+import Types.PlayerStatus exposing (PlayerStatus(..))
+import Types.Score exposing (Score(..))
 
 
-endScreen : Html msg
-endScreen =
+endScreen : AllPlayers -> Html msg
+endScreen players =
     div
         [ Attr.id "endScreen"
         ]
-        [ Html.text "KONEC HRY"
+        [ results players
+        , Html.img [ Attr.id "KONEC_HRY", Attr.src "./resources/konec-hry.png" ] []
         ]
+
+
+results : AllPlayers -> Html msg
+results players =
+    div
+        [ Attr.id "results"
+        ]
+        (players |> Dict.toList |> List.map (Tuple.second >> resultsEntry))
+
+
+resultsEntry : ( Player, PlayerStatus ) -> Html msg
+resultsEntry ( player, status ) =
+    div
+        [ Attr.class "resultsEntry"
+        ]
+        (case status of
+            Participating (Score score) ->
+                GUI.Digits.small player.color score
+
+            NotParticipating ->
+                []
+        )
