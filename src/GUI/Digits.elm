c36d1668ffff78d277edd7f8013137da48a321ed
Simon Alling <alling.simon@gmail.com>
2023-02-19 00:20:26 +0100

    Restore Size

--- src/GUI/Digits.elm
+++ src/GUI/Digits.elm
@@ -6,23 +6,37 @@ import Html exposing (Html, div)
 import Html.Attributes as Attr
 
 
+type Size
+    = Large
+    | Small
+
+
 large : Color -> Int -> List (Html msg)
 large =
-    digits (Scaled 1 GUI.Fonts.bgiStroked28x43)
+    digits Large
 
 
 small : Color -> Int -> List (Html msg)
 small =
-    digits (Scaled 2 GUI.Fonts.bgiDefault8x8)
-
-
-digits : ScaledFont -> Color -> Int -> List (Html msg)
-digits fontAndSize color =
-    String.fromInt >> text fontAndSize color
+    digits Small
 
 
 type ScaledFont
     = Scaled Int Font
+
+
+digits : Size -> Color -> Int -> List (Html msg)
+digits size color =
+    let
+        fontAndSize =
+            case size of
+                Large ->
+                    Scaled 1 GUI.Fonts.bgiStroked28x43
+
+                Small ->
+                    Scaled 2 GUI.Fonts.bgiDefault8x8
+    in
+    String.fromInt >> text fontAndSize color
 
 
 text : ScaledFont -> Color -> String -> List (Html msg)
