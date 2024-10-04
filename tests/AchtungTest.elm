a5f70b7d04c5ef3dde80619fffa61bad9f302f82
Simon Alling <SimonAlling@users.noreply.github.com>
2024-10-04 23:40:21 +0200

    Add test case for crashing into wall (#96)

--- tests/AchtungTest.elm
+++ tests/AchtungTest.elm
@@ -88,4 +88,73 @@ tests =
                     _ ->
                         Expect.fail "Expected in-game app state"
             )
+        , test
+            "A Kurve that crashes into the wall dies"
+            (\_ ->
+                let
+                    currentKurve : Kurve
+                    currentKurve =
+                        { color = Color.white
+                        , id = 5
+                        , controls = ( Set.empty, Set.empty )
+                        , state =
+                            { position = ( 1, 100 )
+                            , direction = Angle pi
+                            , holeStatus = Unholy 60
+                            }
+                        , stateAtSpawn =
+                            { position = ( 100, 100 )
+                            , direction = Angle 0
+                            , holeStatus = Unholy 60
+                            }
+                        , reversedInteractions = []
+                        }
+
+                    currentRound : Round
+                    currentRound =
+                        { kurves =
+                            { alive = [ currentKurve ]
+                            , dead = []
+                            }
+                        , occupiedPixels = Set.empty
+                        , history =
+                            { initialState =
+                                { seedAfterSpawn = Random.initialSeed 0
+                                , spawnedKurves = []
+                                , pressedButtons = Set.empty
+                                }
+                            }
+                        , seed = Random.initialSeed 0
+                        }
+
+                    currentModel : Model
+                    currentModel =
+                        { pressedButtons = Set.empty
+                        , appState = InGame (Active NotPaused (Moving Tick.genesis ( Live, currentRound )))
+                        , config = Config.default
+                        , players = Dict.empty
+                        }
+
+                    newAppState : AppState
+                    newAppState =
+                        currentModel |> update (GameTick (Tick.succ Tick.genesis) ( Live, currentRound )) |> Tuple.first |> .appState
+                in
+                case newAppState of
+                    InGame gameState ->
+                        case gameState of
+                            RoundOver round _ ->
+                                case ( round.kurves.alive, round.kurves.dead ) of
+                                    ( [], kurve :: [] ) ->
+                                        Expect.equal kurve.state.position
+                                            ( 0, 100 )
+
+                                    _ ->
+                                        Expect.fail "Expected exactly one dead Kurve and no alive ones"
+
+                            _ ->
+                                Expect.fail "Expected round to be over"
+
+                    _ ->
+                        Expect.fail "Expected in-game app state"
+            )
         ]
