1b1f93f2f37f8b537ef52ee9b7113dbf44369ed3
Simon Alling <alling.simon@gmail.com>
2025-01-03 15:16:36 +0100

    WebGL hello world example

--- src/WebGlSandbox.elm
+++ src/WebGlSandbox.elm
@@ -1,44 +1,104 @@
 module WebGlSandbox exposing (..)
 
+{-
+   Rotating triangle, that is a "hello world" of the WebGL
+-}
+
 import Browser
+import Browser.Events exposing (onAnimationFrameDelta)
 import Html exposing (Html)
+import Html.Attributes exposing (height, style, width)
+import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector3 as Vec3 exposing (Vec3, vec3)
+import WebGL exposing (Mesh, Shader)
 
 
-type alias Model =
-    {}
-
-
-type Msg
-    = NoOp
-
-
-main : Program () Model Msg
+main : Program () Float Float
 main =
     Browser.element
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
+        { init = \_ -> ( 0, Cmd.none )
         , view = view
+        , subscriptions = \_ -> onAnimationFrameDelta Basics.identity
+        , update = \elapsed currentTime -> ( elapsed + currentTime, Cmd.none )
         }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( {}, Cmd.none )
+view : Float -> Html msg
+view t =
+    WebGL.toHtml
+        [ width 400
+        , height 400
+        , style "display" "block"
+        ]
+        [ WebGL.entity
+            vertexShader
+            fragmentShader
+            mesh
+            { perspective = perspective (t / 1000) }
+        ]
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
+perspective : Float -> Mat4
+perspective t =
+    Mat4.mul
+        (Mat4.makePerspective 45 1 0.01 100)
+        (Mat4.makeLookAt (vec3 (4 * cos t) 0 (4 * sin t)) (vec3 0 0 0) (vec3 0 1 0))
 
 
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+
+-- Mesh
 
 
-view : Model -> Html Msg
-view _ =
-    Html.text "HEJ"
+type alias Vertex =
+    { position : Vec3
+    , color : Vec3
+    }
+
+
+mesh : Mesh Vertex
+mesh =
+    WebGL.triangles
+        [ ( Vertex (vec3 0 0 0) (vec3 1 0 0)
+          , Vertex (vec3 1 1 0) (vec3 0 1 0)
+          , Vertex (vec3 1 -1 0) (vec3 0 0 1)
+          )
+        ]
+
+
+
+-- Shaders
+
+
+type alias Uniforms =
+    { perspective : Mat4 }
+
+
+vertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
+vertexShader =
+    [glsl|
+
+        attribute vec3 position;
+        attribute vec3 color;
+        uniform mat4 perspective;
+        varying vec3 vcolor;
+
+        void main () {
+            gl_Position = perspective * vec4(position, 1.0);
+            vcolor = color;
+        }
+
+    |]
+
+
+fragmentShader : Shader {} Uniforms { vcolor : Vec3 }
+fragmentShader =
+    [glsl|
+
+        precision mediump float;
+        varying vec3 vcolor;
+
+        void main () {
+            gl_FragColor = vec4(vcolor, 1.0);
+        }
+
+    |]
