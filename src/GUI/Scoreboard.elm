e609f05e27c47750f9db3ff8eea0a986bda9e95d
Simon Alling <alling.simon@gmail.com>
2025-01-07 14:48:09 +0100

    Add getCurrentRound function in Game module (#153)
    The `Scoreboard` module shouldn't need to know so much about the internals of the `GameState` type – it only needs the current round.
    
    💡 `git show --color-words='\(content players .+|\w+|.'`

--- src/GUI/Scoreboard.elm
+++ src/GUI/Scoreboard.elm
@@ -2,7 +2,7 @@ module GUI.Scoreboard exposing (scoreboard)
 
 import Dict
 import GUI.Digits
-import Game exposing (ActiveGameState(..), GameState(..))
+import Game exposing (GameState)
 import Html exposing (Html, div)
 import Html.Attributes as Attr
 import Players exposing (AllPlayers, includeResultsFrom, participating)
@@ -18,16 +18,7 @@ scoreboard gameState players =
         [ Attr.id "scoreboard"
         , Attr.class "canvasHeight"
         ]
-        (case gameState of
-            Active _ (Spawning _ ( _, round )) ->
-                content players round
-
-            Active _ (Moving _ ( _, round )) ->
-                content players round
-
-            RoundOver round _ ->
-                content players round
-        )
+        (content players (Game.getCurrentRound gameState))
 
 
 content : AllPlayers -> Round -> List (Html msg)
