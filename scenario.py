846f8c62bcac298f2228dfa22c19dbb81d5545de
Simon Alling <alling.simon@gmail.com>
2025-10-24 17:08:52 +0200

    Scenario DSL kind of works

--- scenario.py
+++ scenario.py
@@ -2,6 +2,7 @@
 # This script typically needs to be run as root.
 # Usage: sudo scenario.py `pgrep dosbox`
 
+import math
 import subprocess
 import sys
 
@@ -14,12 +15,64 @@ GREEN = 3
 PINK = 4
 BLUE = 5
 
+SIZEOF_FLOAT = 4
+
 BASE_ADDRESS = 0x7fffd8010ff6
 
 X_COORDS_ADDRESS = BASE_ADDRESS
 Y_COORDS_ADDRESS = BASE_ADDRESS + 24
 DIRECTIONS_ADDRESS = BASE_ADDRESS + 48
 
-scanmem_command = f"option endianness 1;write float32 {hex(BASE_ADDRESS)} 450;exit"
+def write_float32(address: int, value: float) -> str:
+    return f"write float32 {hex(address)} {value}"
+
+def set_x(player_id: int, x: float) -> str:
+    return write_float32(X_COORDS_ADDRESS + player_id * SIZEOF_FLOAT, x)
+
+def set_y(player_id: int, y: float) -> str:
+    return write_float32(Y_COORDS_ADDRESS + player_id * SIZEOF_FLOAT, y)
+
+def set_position(player_id: int, x: float, y: float) -> str:
+    return sequence([
+        set_x(player_id, x),
+        set_y(player_id, y),
+    ])
+
+def set_direction_raw(player_id: int, direction: float) -> str:
+    return write_float32(DIRECTIONS_ADDRESS + player_id * SIZEOF_FLOAT, direction)
+
+def set_direction_conventional(player_id: int, conventional_direction: float) -> str:
+    return set_direction_raw(player_id, conventional_direction + math.pi / 2)
+
+def sequence(commands: list[str]) -> str:
+    return ";".join(commands)
+
+SETUP_COMMANDS: list[str] = [
+    "option endianness 1",
+]
+
+SCENARIO_COMMANDS: list[str] = [
+    set_position(RED, 50, 50),
+    set_direction_conventional(RED, 0),
+    set_position(YELLOW, 50, 100),
+    set_direction_conventional(YELLOW, 0),
+    set_position(GREEN, 50, 150),
+    set_direction_conventional(GREEN, 0),
+]
+
+TEARDOWN_COMMANDS: list[str] = [
+    "exit",
+]
+
+all_commands: list[str] = SETUP_COMMANDS + SCENARIO_COMMANDS + TEARDOWN_COMMANDS
+
+scanmem_command: str = sequence(all_commands)
+
+print("BEGIN SCANMEM PROGRAM")
+print()
+print("    ", scanmem_command)
+print()
+print("END SCANMEM PROGRAM")
+print()
 
 subprocess.run([ "scanmem", process_id, "--errexit", "--command", scanmem_command ])
