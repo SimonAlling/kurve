a93467c23c5bfef1ac4bf3438327cf882d7103aa
Simon Alling <alling.simon@gmail.com>
2025-11-01 16:57:46 +0100

    Rename script

--- tools/show-game-state.sh
+++ tools/show-game-state.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-sudo scanmem `pgrep dosbox` --errexit --command "option dump_with_ascii 0;dump 0x7fffd8010ff6 72;exit" 2>/dev/null | $(dirname $0)/parse-scanmem-output.py
+sudo scanmem `pgrep dosbox` --errexit --command "option dump_with_ascii 0;dump 0x7fffd8010ff6 72;exit" 2>/dev/null | $(dirname $0)/interpret-scanmem-output.py
