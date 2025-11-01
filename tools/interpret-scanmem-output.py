6bcfd00e673b40a107e42c88a94b9503962b02ed
Simon Alling <alling.simon@gmail.com>
2025-11-01 17:08:26 +0100

    Add script for showing game state (#170)
    This PR adds a script that prints the internal game state in a human-readable way:
    
                   x                    y                    Direction (0 = down)
        🟥 Red     191.18063354492188   137.90963745117188   ↑ 2.992500066757202
        🟨 Yellow  0.0                  0.0                  ↓ 0.0
        🟧 Orange  0.0                  0.0                  ↓ 0.0
        🟩 Green   448.3548583984375    290.88726806640625   ↗ 2.547300100326538
        🟪 Pink    0.0                  0.0                  ↓ 0.0
        🟦 Blue    0.0                  0.0                  ↓ 0.0
    
    Suggested usage:
    
        watch -n 0.1 ./tools/show-game-state.sh
    
    Then, in another terminal, run the game as usual and/or stage a scenario with `./tools/scenario.py docs/original-game/ZATACKA.EXE`. The `watch` command above should automatically start showing the game state every time the game is (re)started.
    
    ## Prerequisites
    
    The prerequisites are the same as in #164 and #165:
    
      * ASLR must be disabled.
      * `memsize=1` must be set in the DOSBox config.
      * scanmem must be installed.
      * xdotool is needed for scenario automation.
    
    ## Limitations
    
    No particular timing accuracy should be expected. `watch -n 0.1` alone updates less frequently than the game itself, and there might be yet other inaccuracy sources. For more precise real-time understanding of the internal game state, slow down the emulation by spamming Ctrl + F11 in DOSBox. (That can be done in `scenario.py` by spamming `press_key("Ctrl+F11")`.) The slower the emulation, the more accurate the output of the script.
    
    Note also that nothing related to holes is shown, since we haven't yet figured out how holes are implemented (#168).

---
+++ tools/interpret-scanmem-output.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+from math import pi
+import re
+import struct
+import sys
+
+HEX_BYTE_RE = re.compile(r"\b[0-9A-Fa-f]{2}\b")
+
+PLAYERS = [
+    ("🟥", "Red"),
+    ("🟨", "Yellow"),
+    ("🟧", "Orange"),
+    ("🟩", "Green"),
+    ("🟪", "Pink"),
+    ("🟦", "Blue"),
+]
+NUMBER_OF_PLAYERS = len(PLAYERS)
+
+COLUMN_WIDTH = 20
+
+
+def parse_dump(text: str) -> bytes:
+    all_bytes: list[int] = []
+
+    for line in text.splitlines():
+        for token in HEX_BYTE_RE.findall(line):
+            all_bytes.append(int(token, 16))
+
+    return bytes(all_bytes)
+
+
+def to_float32_le(b: bytes) -> float:
+    return struct.unpack("<f", b)[0]
+
+
+def chunk_bytes(bs: bytes, n: int) -> list[bytes]:
+    return [bs[i : i + n] for i in range(0, len(bs), n) if len(bs[i : i + n]) == n]
+
+
+def chunk_floats(bs: list[float], n: int) -> list[list[float]]:
+    return [bs[i : i + n] for i in range(0, len(bs), n) if len(bs[i : i + n]) == n]
+
+
+ARROWS = [
+    "↓",
+    "↘",
+    "→",
+    "↗",
+    "↑",
+    "↖",
+    "←",
+    "↙",
+]
+NUMBER_OF_ARROWS = len(ARROWS)
+
+
+def arrow_for_dir(
+    raw_direction: float,
+) -> str:
+    aligned_direction = (
+        # The angle "almost 2π" should be represented by the downward arrow, i.e. index 0, not index n - 1. This addition "pushes it over the edge". The addition will be negated below by rounding down.
+        raw_direction + pi / NUMBER_OF_ARROWS
+    )
+    cycle = 2 * pi
+    arrow_index = int((aligned_direction % cycle) / cycle * NUMBER_OF_ARROWS)
+    return ARROWS[arrow_index]
+
+
+def main():
+    text: str = sys.stdin.read()
+
+    if text == "":
+        print("⚠️  Empty input on stdin. Is the game running?")
+        sys.exit(1)
+
+    raw_bytes: bytes = parse_dump(text)
+
+    values: list[float] = []
+    offset: int = 0
+
+    for quad in chunk_bytes(raw_bytes, 4):
+        try:
+            f = to_float32_le(quad)
+        except Exception:
+            f = float("nan")
+        values.append(f)
+        offset += 4
+
+    [xs, ys, dirs] = chunk_floats(values, NUMBER_OF_PLAYERS)
+
+    # Table head:
+    print(
+        "          ",
+        "x".ljust(COLUMN_WIDTH),
+        "y".ljust(COLUMN_WIDTH),
+        "Direction (0 = down)".ljust(COLUMN_WIDTH),
+    )
+
+    # Table body:
+    for player_id in range(0, NUMBER_OF_PLAYERS):
+        x = xs[player_id]
+        y = ys[player_id]
+        dir = dirs[player_id]
+        print(
+            PLAYERS[player_id][0],
+            PLAYERS[player_id][1].ljust(7),
+            str(x).ljust(COLUMN_WIDTH),
+            str(y).ljust(COLUMN_WIDTH),
+            arrow_for_dir(dir),
+            str(dir).ljust(COLUMN_WIDTH),
+        )
+
+
+if __name__ == "__main__":
+    main()
