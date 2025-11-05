7f162288bc1e8822f6b65a09167c1f8ecd953218
Simon Alling <alling.simon@gmail.com>
2025-11-05 22:53:36 +0100

    Un-hardcode base address in tools

--- tools/show-game-state.sh
+++ tools/show-game-state.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-sudo scanmem `pgrep dosbox` --errexit --command "option dump_with_ascii 0;dump 0x7fffd8010ff6 72;exit" 2>/dev/null | $(dirname $0)/interpret-scanmem-output.py
+USAGE="For example: $0 7fffd8010ff6"
+
+base_address="${1:?Please specify base address. $USAGE}"
+
+sudo scanmem `pgrep dosbox` --errexit --command "option dump_with_ascii 0;dump ${base_address} 72;exit" 2>/dev/null | $(dirname $0)/interpret-scanmem-output.py
