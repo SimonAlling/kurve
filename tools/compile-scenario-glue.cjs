9ae7f553e32905b481639ae59805c782c596e795
Simon Alling <alling.simon@gmail.com>
2025-11-16 19:33:18 +0100

    Simplify glue JavaScript

--- tools/compile-scenario-glue.cjs
+++ tools/compile-scenario-glue.cjs
@@ -6,10 +6,7 @@ try {
         flags: { elmFlag_commandLineArgs: process.argv.slice(2) },
     });
 
-    app.ports.outputToOutsideWorld.subscribe((outputFromElm) => {
-        console.log(outputFromElm);
-        process.exit(0);
-    });
+    app.ports.outputToOutsideWorld.subscribe(console.log);
 } catch (caught) {
     console.error("Elm initialization failed.");
     console.error(String(caught));
