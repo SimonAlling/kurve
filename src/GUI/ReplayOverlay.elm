d8d0c1fb1e737e25791e97ce1274d6cb26bdda82
Simon Alling <alling.simon@gmail.com>
2025-12-13 01:36:22 +0100

    Don't show full replay text when paused

--- src/GUI/ReplayOverlay.elm
+++ src/GUI/ReplayOverlay.elm
@@ -2,7 +2,7 @@ module GUI.ReplayOverlay exposing (replayOverlay)
 
 import Color
 import GUI.Text as Text
-import Game exposing (ActiveGameState(..), GameState(..), MidRoundStateVariant(..))
+import Game exposing (ActiveGameState(..), GameState(..), MidRoundStateVariant(..), Paused(..))
 import Html exposing (Html)
 import Html.Attributes as Attr
 
@@ -19,8 +19,14 @@ replayOverlay gameState =
 content : GameState -> Html msg
 content gameState =
     case gameState of
-        Active _ (Spawning _ ( Replay, _ )) ->
-            fullTextInTheMiddle
+        Active paused (Spawning _ ( Replay, _ )) ->
+            case paused of
+                Paused ->
+                    -- Cannot show replay text when paused, because it's rendered on top of pause text.
+                    nothing
+
+                NotPaused ->
+                    fullTextInTheMiddle
 
         Active _ (Moving _ _ ( Replay, _ )) ->
             singleLetterInTheCorner
