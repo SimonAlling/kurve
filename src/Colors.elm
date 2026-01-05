590b338034d3d74896fe1ea96d8a3f6353575f60
Simon Alling <alling.simon@gmail.com>
2026-01-05 22:42:40 +0100

    Bring back rgb alias for Color.rgb255

--- src/Colors.elm
+++ src/Colors.elm
@@ -15,44 +15,49 @@ import Color exposing (Color)
 -}
 red : Color
 red =
-    Color.rgb255 255 40 0
+    rgb 255 40 0
 
 
 {-| The color of the yellow player in the original game.
 -}
 yellow : Color
 yellow =
-    Color.rgb255 195 195 0
+    rgb 195 195 0
 
 
 {-| The color of the orange player in the original game.
 -}
 orange : Color
 orange =
-    Color.rgb255 255 121 0
+    rgb 255 121 0
 
 
 {-| The color of the green player in the original game.
 -}
 green : Color
 green =
-    Color.rgb255 0 203 0
+    rgb 0 203 0
 
 
 {-| The color of the pink player in the original game.
 -}
 pink : Color
 pink =
-    Color.rgb255 223 81 182
+    rgb 223 81 182
 
 
 {-| The color of the blue player in the original game.
 -}
 blue : Color
 blue =
-    Color.rgb255 0 162 203
+    rgb 0 162 203
 
 
 white : Color
 white =
-    Color.rgb255 255 255 255
+    rgb 255 255 255
+
+
+rgb : Int -> Int -> Int -> Color
+rgb =
+    Color.rgb255
