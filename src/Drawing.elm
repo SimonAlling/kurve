d101f64f884da41fb2732a02af0a14fcc98e6871
Simon Alling <alling.simon@gmail.com>
2026-01-27 22:24:08 +0100

    Make drawing a lot in a single frame more efficient

--- src/Drawing.elm
+++ src/Drawing.elm
@@ -1,4 +1,4 @@
-module Drawing exposing (WhatToDraw, drawSpawnsPermanently, drawSpawnsTemporarily, getColorAndDrawingPosition, mergeWhatToDraw)
+module Drawing exposing (DrawingAccumulator, WhatToDraw, accumulate, drawSpawnsPermanently, drawSpawnsTemporarily, finalize, getColorAndDrawingPosition, initialize, mergeWhatToDraw)
 
 import Color exposing (Color)
 import Types.Kurve exposing (Kurve)
@@ -9,6 +9,48 @@ type alias WhatToDraw =
     { headDrawing : List ( Color, DrawingPosition )
     , bodyDrawing : List ( Color, DrawingPosition )
     }
+
+
+{-| Used to efficiently accumulate what to draw, even in extremely long frames (such as when fast-forwarding/rewinding a replay).
+-}
+type DrawingAccumulator
+    = Empty
+    | Accum
+        { headDrawing : List ( Color, DrawingPosition )
+        , reversedBodyDrawings : List (List ( Color, DrawingPosition ))
+        }
+
+
+initialize : DrawingAccumulator
+initialize =
+    Empty
+
+
+accumulate : DrawingAccumulator -> WhatToDraw -> DrawingAccumulator
+accumulate acc new =
+    Accum
+        { headDrawing = new.headDrawing
+        , reversedBodyDrawings =
+            case acc of
+                Empty ->
+                    [ new.bodyDrawing ]
+
+                Accum accumulated ->
+                    new.bodyDrawing :: accumulated.reversedBodyDrawings
+        }
+
+
+finalize : DrawingAccumulator -> Maybe WhatToDraw
+finalize acc =
+    case acc of
+        Empty ->
+            Nothing
+
+        Accum accumulated ->
+            Just
+                { headDrawing = accumulated.headDrawing
+                , bodyDrawing = accumulated.reversedBodyDrawings |> List.reverse |> List.concat
+                }
 
 
 mergeWhatToDraw : WhatToDraw -> WhatToDraw -> WhatToDraw
