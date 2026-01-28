9deca43675537fbe32062c71f8564f121b4d22c4
Simon Alling <alling.simon@gmail.com>
2026-01-28 17:35:31 +0100

    Clarify comment

--- src/World.elm
+++ src/World.elm
@@ -128,7 +128,7 @@ isCloseTo ( x_front, y_front ) ( x_candidate, y_candidate ) =
     squareDistanceBetweenPoints <= maxSquareDistance
 
 
-{-| Must be at least 5/4 (inclusive) and at most 9/4 (exclusive); see the PR that added this comment.
+{-| Must be at least 5/4 (inclusive) and at most (3/2)² = 9/4 (exclusive); see the PR that added this comment.
 -}
 maxSquareDistance : Float
 maxSquareDistance =
