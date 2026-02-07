SimonAlling/kurve

commit d590e99424c23211a4b927b0c2cdfb4e076ca9c2

Replicate off-by-one error at top and left wall (#263)
As briefly mentioned in #262, our understanding is that the original game derives drawing positions not by rounding down, but by _truncating_ – i.e. effectively just removing the decimal parts of the floating-point components of the internal position. That results in counterintuitive behavior at the top and left walls, as made obvious by this scenario:

```elm
theScenario : Scenario
theScenario =
    [ ( Red
      , { x = 50
        , y = 3
        , direction = pi / 2 + 0.01
        }
      )
    , ( Green
      , { x = 50
        , y = 100
        , direction = pi / 2
        }
      )
    ]
```

Note that Red moves ~100 pixels parallel to the wall, then "snaps" up 1 pixel, then moves another ~100 pixels, then "snaps" up again – and then moves about _200 pixels_ before crashing. Without deep familiarity with the original game, one would expect it to crash much sooner.

For illustrative purposes, consider the above scenario, but with `0.2` instead of `0.01`. You'd be forgiven for expecting it to end something like this:

    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛⬛⬛⬛⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛

But it ends like this:

    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛
    ⬛⬛⬛⬛⬛⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛
    ⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛
    ⬛⬛🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛🟥🟥🟥🟥🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛

The reason is that coordinates in the interval (0, –1) are rounded to 0 for hit detection and drawing. So it's effectively possible for a Kurve to be slightly outside the top and left boundaries of the canvas, but that's _rendered_ as if it's completely within the canvas.

💡 `git show --color-words='floor|[0-9]+|.'`

Co-authored-by: Simon Lydell <simon.lydell@gmail.com>

tests/AchtungTest.elm
@@ -109,6 +109,9 @@ crashingIntoWallTests =
 When a Kurve is traveling almost horizontally or vertically, it very obviously "snaps over to the next pixel row/column" at regular intervals.
 When approaching a horizontal or vertical obstacle (wall or Kurve) from a shallow angle, an experienced player can easily tell based on the "snaps" exactly when they need to turn away to avoid crashing, because that will always happen at a "snap", never in the middle of a continuous "segment".
 
+However, the top and left walls are exceptions to the rule in the sense that the player can enjoy an _extra_ segment before crashing.
+That is, it's effectively possible to be up to (but not including) 1 pixel _outside_ the canvas at the top and left borders, but the _rendered_ Kurve and its hitbox are always fully within the canvas.
+
 For example, the illustration below shows the exact moment when Green crashes into Red.
 
 Notably, Green enjoys a full "segment" right next to Red before dying.

src/TestScenarios/AroundTheWorld.elm
@@ -43,7 +43,7 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 2011
+    { tickThatShouldEndIt = tickNumber 2012
     , howItShouldEnd =
         { aliveAtTheEnd = []
         , deadAtTheEnd =

src/TestScenarios/CrashIntoWallBasic.elm
@@ -26,7 +26,7 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 2
+    { tickThatShouldEndIt = tickNumber 3
     , howItShouldEnd =
         { aliveAtTheEnd = []
         , deadAtTheEnd =

src/TestScenarios/CrashIntoWallExactTiming.elm
@@ -26,12 +26,12 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 251
+    { tickThatShouldEndIt = tickNumber 351
     , howItShouldEnd =
         { aliveAtTheEnd = []
         , deadAtTheEnd =
             [ { id = playerIds.green
-              , theDrawingPositionItNeverMadeItTo = { leftEdge = 350, topEdge = -1 }
+              , theDrawingPositionItNeverMadeItTo = { leftEdge = 450, topEdge = -1 }
               }
             ]
         }

src/TestScenarios/CrashIntoWallLeft.elm
@@ -26,7 +26,7 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 2
+    { tickThatShouldEndIt = tickNumber 3
     , howItShouldEnd =
         { aliveAtTheEnd = []
         , deadAtTheEnd =

src/TestScenarios/CrashIntoWallTop.elm
@@ -26,7 +26,7 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 2
+    { tickThatShouldEndIt = tickNumber 3
     , howItShouldEnd =
         { aliveAtTheEnd = []
         , deadAtTheEnd =

src/TestScenarios/CuttingCornersBasic.elm
@@ -39,12 +39,12 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 277
+    { tickThatShouldEndIt = tickNumber 278
     , howItShouldEnd =
         { aliveAtTheEnd = [ { id = playerIds.red } ]
         , deadAtTheEnd =
             [ { id = playerIds.green
-              , theDrawingPositionItNeverMadeItTo = { leftEdge = 295, topEdge = -1 }
+              , theDrawingPositionItNeverMadeItTo = { leftEdge = 296, topEdge = -1 }
               }
             ]
         }

src/TestScenarios/CuttingCornersPerfectOverpainting.elm
@@ -65,12 +65,12 @@ spawnedKurves =
 
 expectedOutcome : RoundOutcome
 expectedOutcome =
-    { tickThatShouldEndIt = tickNumber 138
+    { tickThatShouldEndIt = tickNumber 140
     , howItShouldEnd =
         { aliveAtTheEnd = [ { id = playerIds.orange } ]
         , deadAtTheEnd =
             [ { id = playerIds.green
-              , theDrawingPositionItNeverMadeItTo = { leftEdge = 116, topEdge = -1 }
+              , theDrawingPositionItNeverMadeItTo = { leftEdge = 117, topEdge = -1 }
               }
             , { id = playerIds.yellow
               , theDrawingPositionItNeverMadeItTo = { leftEdge = 58, topEdge = 58 }

commit c8af228eaf47c7e97ead62fa282317f769fda2b3

Fix edge-case drawing bug at end of hole (#331)
While playing the game, @lydell and I ran into a situation where the Kurve's head disappeared when crashing into the top wall.

We initially thought it had something to do with the truncation/off-by-one error at the top and left wall that we deliberately introduced in #263. And it _kind of_ does, but the truncation error just triggers a special case of a more general issue, namely that **we don't draw anything if the drawing position is the same as in the previous tick**.

I've identified two ways that can happen:

  * The Kurve stayed within the same pixel as in the previous tick. For example, it moved from (100.1, 100.1) to roughly (100.8, 100.8).
  * The Kurve crossed the top or left edge of the canvas at an angle roughly orthogonal to the edge. For example, it moved from (100, 0.5) to (100, –0.5).

In both cases, the new position is mapped to the same drawing position as the old position, so `checkedPositionsReversed` will be the empty list. So if the Kurve lives and is solid at the new position, we will not draw anything. That's problematic when something like this happens:

| Position       | Holiness | Fate  | Draws                     |
|----------------|----------|-------|---------------------------|
| (100.1, 477.1) | Holy     | Lives | No; holy                  |
| (100.8, 477.8) | Solid    | Lives | No; same drawing position |
| (101.5, 478.5) | Solid    | Dies  | No; dies                  |

The result is that the head seems to disappear in thin air, instead of being permanently drawn at the last position where the Kurve was able to be.

This PR includes two test cases representing said variations of this problem. Note that the `_Top` one "relies" on the truncation error, while the `_Bottom` one doesn't.

💡 `git show --ignore-space-change`

src/Game.elm
@@ -316,8 +316,20 @@ evaluateMove config startingPoint desiredEndPoint occupiedPixels holinessTransit
                     []
 
                 ( Lives, Solid ) ->
-                    -- The Kurve lives and is solid. Draw everything it wanted to draw.
-                    checkedPositionsReversed
+                    case oldHoliness of
+                        Holy ->
+                            -- The Kurve lives and just became solid. Draw everything it wanted to draw.
+                            -- If the Kurve didn't visually move at all in this tick, then it should still be drawn.
+                            -- There are at least two ways this can happen; see the PR/commit that added this comment.
+                            if List.isEmpty checkedPositionsReversed then
+                                List.singleton startingPointAsDrawingPosition
+
+                            else
+                                checkedPositionsReversed
+
+                        Solid ->
+                            -- The Kurve lives and is solid. Draw everything it wanted to draw.
+                            checkedPositionsReversed
 
                 ( Dies, Holy ) ->
                     case oldHoliness of

src/TestScenarios/CrashingAfterBecomingSolidAtSameDrawingPosition_Bottom.elm
@@ -1,0 +1,121 @@
+module TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Bottom exposing (config, expectedOutcome, spawnedKurves)
+
+import Colors
+import Config exposing (Config)
+import Effect exposing (Effect(..))
+import Holes exposing (HoleStatus(..), Holiness(..))
+import Random
+import TestScenarioHelpers exposing (EffectsExpectation(..), RoundOutcome, makeZombieKurve, playerIds, tickNumber)
+import Types.Angle exposing (Angle(..))
+import Types.Kurve exposing (Kurve)
+
+
+config : Config
+config =
+    Config.default
+        |> Config.withHardcodedHoles 100 4
+
+
+green : Kurve
+green =
+    makeZombieKurve
+        { color = Colors.green
+        , id = playerIds.green
+        , state =
+            { position = ( 103.6, 473.6 )
+            , direction = Angle (pi / 4)
+            , holeStatus =
+                RandomHoles
+                    { holiness = Solid
+                    , ticksLeft = 1
+                    , holeSeed = Random.initialSeed 0
+                    }
+            }
+        }
+
+
+spawnedKurves : List Kurve
+spawnedKurves =
+    [ green ]
+
+
+expectedOutcome : RoundOutcome
+expectedOutcome =
+    { tickThatShouldEndIt = tickNumber 7
+    , howItShouldEnd =
+        { aliveAtTheEnd = []
+        , deadAtTheEnd =
+            [ { id = playerIds.green
+              , theDrawingPositionItNeverMadeItTo = { x = 108, y = 478 }
+              }
+            ]
+        }
+    , effectsItShouldProduce =
+        ExpectEffects
+            [ -- Spawning:
+              DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 103, y = 473 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 103, y = 473 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 103, y = 473 } ) ]
+                }
+
+            -- Draw spawn position permanently:
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 103, y = 473 } ) ]
+                , headDrawing = []
+                }
+
+            -- Start moving:
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 104, y = 474 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 104, y = 474 } ) ]
+                }
+
+            -- Start of hole:
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 105, y = 475 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 105, y = 475 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 106, y = 476 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 107, y = 477 } ) ]
+                }
+
+            -- Start of solid segment (note that drawing position is same as previous one):
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 107, y = 477 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 107, y = 477 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            ]
+    }

src/TestScenarios/CrashingAfterBecomingSolidAtSameDrawingPosition_Top.elm
@@ -1,0 +1,149 @@
+module TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Top exposing (config, expectedOutcome, spawnedKurves)
+
+import Colors
+import Config exposing (Config)
+import Effect exposing (Effect(..))
+import Holes exposing (HoleStatus(..), Holiness(..))
+import Random
+import TestScenarioHelpers exposing (EffectsExpectation(..), RoundOutcome, makeZombieKurve, playerIds, tickNumber)
+import Types.Angle exposing (Angle(..))
+import Types.Kurve exposing (Kurve)
+
+
+config : Config
+config =
+    Config.default
+        |> Config.withHardcodedHoles 100 8
+
+
+green : Kurve
+green =
+    makeZombieKurve
+        { color = Colors.green
+        , id = playerIds.green
+        , state =
+            { position = ( 100, 12.1 )
+            , direction = Angle pi
+            , holeStatus =
+                RandomHoles
+                    { holiness = Solid
+                    , ticksLeft = 4
+                    , holeSeed = Random.initialSeed 0
+                    }
+            }
+        }
+
+
+spawnedKurves : List Kurve
+spawnedKurves =
+    [ green ]
+
+
+expectedOutcome : RoundOutcome
+expectedOutcome =
+    { tickThatShouldEndIt = tickNumber 14
+    , howItShouldEnd =
+        { aliveAtTheEnd = []
+        , deadAtTheEnd =
+            [ { id = playerIds.green
+              , theDrawingPositionItNeverMadeItTo = { x = 100, y = -1 }
+              }
+            ]
+        }
+    , effectsItShouldProduce =
+        ExpectEffects
+            [ -- Spawning:
+              DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 12 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 12 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 12 } ) ]
+                }
+
+            -- Draw spawn position permanently:
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 100, y = 12 } ) ]
+                , headDrawing = []
+                }
+
+            -- Start moving:
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 100, y = 11 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 100, y = 11 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 100, y = 10 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 100, y = 10 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 100, y = 9 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 100, y = 9 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 100, y = 8 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 100, y = 8 } ) ]
+                }
+
+            -- Start of hole:
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 7 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 6 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 5 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 4 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 3 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 2 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 1 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = [ ( Colors.green, { x = 100, y = 0 } ) ]
+                }
+
+            -- Start of solid segment (note that drawing position is same as previous one):
+            , DrawSomething
+                { bodyDrawing = [ ( Colors.green, { x = 100, y = 0 } ) ]
+                , headDrawing = [ ( Colors.green, { x = 100, y = 0 } ) ]
+                }
+            , DrawSomething
+                { bodyDrawing = []
+                , headDrawing = []
+                }
+            ]
+    }

tests/AchtungTest.elm
@@ -14,6 +14,8 @@ import TestScenarios.CrashIntoWallLeft
 import TestScenarios.CrashIntoWallRight
 import TestScenarios.CrashIntoWallTop
 import TestScenarios.CrashSomewhatSoon
+import TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Bottom
+import TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Top
 import TestScenarios.CrashingWhileBecomingSolid
 import TestScenarios.CuttingCornersBasic
 import TestScenarios.CuttingCornersPerfectOverpainting
@@ -244,6 +246,18 @@ holeTests =
                     |> expectRoundOutcome
                         TestScenarios.CrashingWhileBecomingSolid.config
                         TestScenarios.CrashingWhileBecomingSolid.expectedOutcome
+        , test "First solid position is permanently drawn when becoming solid without changing drawing position and then immediately crashing" <|
+            \_ ->
+                roundWith TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Bottom.spawnedKurves
+                    |> expectRoundOutcome
+                        TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Bottom.config
+                        TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Bottom.expectedOutcome
+        , test "First solid position is permanently drawn when becoming solid just outside the canvas (at effectively the same drawing position as the previous one)" <|
+            \_ ->
+                roundWith TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Top.spawnedKurves
+                    |> expectRoundOutcome
+                        TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Top.config
+                        TestScenarios.CrashingAfterBecomingSolidAtSameDrawingPosition_Top.expectedOutcome
         , test "Hole size and spacing are correct" <|
             \_ ->
                 roundWith TestScenarios.HoleSizeAndSpacing.spawnedKurves